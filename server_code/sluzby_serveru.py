fc62d73ca7212e5aea0e5cd2c9fd1a7c94f3a97a
anvil.election653@passmail.net <anvil.election653@passmail.net>
2025-01-27 21:46:55 +0000

    Created sluzby_serveru; Edited Administrace_komp, Analyza_ahp_komp, Analyza_vystup_komp, Hlavni_okno and 9 more

---
+++ server_code/sluzby_serveru.py
@@ -0,0 +1,39 @@
+import datetime
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+#
+# To allow anvil.server.call() to call functions here, we mark
+# them with @anvil.server.callable.
+
+#@anvil.server.callable
+# def moje_analyzy():
+#   uzivatel = anvil.users.get_user()
+#   if not uzivatel:
+#     return []
+
+#   soupis_analyz = app_tables.Analyza.search(tables.order_by("datum_vytvoreni", ascending=True), User=uzivatel)
+
+#   return soupis_analyz
+
+@anvil.server.callable
+def pridej_analyzu(nazev, popis, zvolena_metoda):
+  datum_vytvoreni = datetime.datetime.now()
+  uzivatel = anvil.users.get_user()
+  datum_upravy = None
+  stav = None
+  if not uzivatel:
+    raise Exception("Neznámí uživatel nemuže volat metodu pridej_analyzu")
+    
+  app_tables.analyza.add_row(nazev=nazev, 
+                            popis=popis, 
+                            datum_vytvoreni=datum_vytvoreni,
+                            zvolena_metoda=zvolena_metoda, 
+                            uzivatel=uzivatel,
+                            datum_upravy=datum_upravy,
+                            stav=stav)
