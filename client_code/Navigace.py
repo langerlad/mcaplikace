24d39e42111b4164d76db12001ba5e9f6cfd6b15
anvil.election653@passmail.net <anvil.election653@passmail.net>
2025-02-15 16:13:36 +0000

    Edited Navigace

--- client_code/Navigace.py
+++ client_code/Navigace.py
@@ -20,7 +20,152 @@ from .Vyber_analyzy_komp import Vyber_analyzy_komp
 # Komponenta hlavního okna
 komponenta_hl_okna = None
 
-# =============== Pomocné funkce ===============
+# Konfigurace stránek a navigace
+KONFIGURACE_NAVIGACE = {
+    'domu': {
+        'komponenta': HERO_komp,
+        'dashboard_komponenta': Dashboard_uziv_komp,
+        'vyzaduje_prihlaseni': False,
+        'oznaceni_nav': 'domu',
+        'kontrola_rozpracovane': True
+    },
+    'pridat_analyzu': {
+        'komponenta': Vyber_analyzy_komp,
+        'vyzaduje_prihlaseni': True,
+        'oznaceni_nav': 'pridat',
+        'kontrola_rozpracovane': True
+    },
+    'uprava_analyzy': {
+        'komponenta': Wizard_komp,
+        'vyzaduje_prihlaseni': True,
+        'oznaceni_nav': 'pridat',
+        'kontrola_rozpracovane': False,
+        'parametry': {'mode': Konstanty.STAV_ANALYZY['UPRAVA']}
+    },
+    'nastaveni': {
+        'komponenta': Nastaveni_komp,
+        'vyzaduje_prihlaseni': True,
+        'oznaceni_nav': 'nastaveni',
+        'kontrola_rozpracovane': True
+    },
+    'info': {
+        'komponenta': Info_komp,
+        'vyzaduje_prihlaseni': False,
+        'oznaceni_nav': 'info',
+        'kontrola_rozpracovane': True
+    },
+    'administrace': {
+        'komponenta': Administrace_komp,
+        'vyzaduje_prihlaseni': True,
+        'oznaceni_nav': 'administrace',
+        'kontrola_rozpracovane': True
+    },
+    'ucet': {
+        'komponenta': Ucet_komp,
+        'vyzaduje_prihlaseni': True,
+        'oznaceni_nav': 'ucet',
+        'kontrola_rozpracovane': True
+    },
+    'ahp': {
+        'komponenta': Analyza_ahp_komp,
+        'vyzaduje_prihlaseni': True,
+        'oznaceni_nav': None,
+        'kontrola_rozpracovane': False
+    },
+    'saw': {
+        'komponenta': Wizard_komp,
+        'vyzaduje_prihlaseni': True,
+        'oznaceni_nav': None,
+        'kontrola_rozpracovane': False
+    }
+}
+
+# Veřejné navigační funkce - nyní jen jednoduché wrappery
+def go_domu():
+    """Přejde na hlavní stránku."""
+    naviguj('domu')
+
+def go_pridat_analyzu():
+    """Přejde na stránku pro přidání nové analýzy."""
+    naviguj('pridat_analyzu')
+
+def go_upravit_analyzu():
+    """Přejde na stránku pro úpravu analýzy."""
+    naviguj('uprava_analyzy')
+
+def go_nastaveni():
+    """Přejde na stránku nastavení."""
+    naviguj('nastaveni')
+
+def go_info():
+    """Přejde na informační stránku."""
+    naviguj('info')
+
+def go_administrace():
+    """Přejde na stránku administrace."""
+    naviguj('administrace')
+
+def go_ucet():
+    """Přejde na stránku uživatelského účtu."""
+    naviguj('ucet')
+
+def go_ahp():
+    """Přejde na stránku AHP analýzy."""
+    naviguj('ahp')
+
+def go_saw():
+    """Přejde na stránku SAW analýzy."""
+    naviguj('saw')
+
+def naviguj(stranka, **parametry):
+    """
+    Centrální navigační funkce.
+    
+    Args:
+        stranka: Identifikátor cílové stránky
+        parametry: Volitelné parametry pro komponentu
+    """
+    try:
+        # Získání konfigurace stránky
+        konfig = KONFIGURACE_NAVIGACE.get(stranka)
+        if not konfig:
+            raise ValueError(f"Neznámá stránka: {stranka}")
+            
+        # Kontrola přihlášení
+        if konfig['vyzaduje_prihlaseni']:
+            uzivatel = kontrola_prihlaseni()
+            if not uzivatel:
+                naviguj('domu')
+                return
+                
+        # Kontrola rozpracované analýzy
+        if konfig['kontrola_rozpracovane']:
+            if not over_a_smaz_rozpracovanou(stranka):
+                return
+                
+        # Nastavení aktivní položky v navigaci
+        if konfig['oznaceni_nav']:
+            komp = ziskej_komponentu()
+            komp.set_active_nav(konfig['oznaceni_nav'])
+            
+        # Speciální případy
+        if stranka == 'domu':
+            komp = ziskej_komponentu()
+            uzivatel = Sprava_dat.je_prihlasen()
+            komponenta = konfig['dashboard_komponenta'] if uzivatel else konfig['komponenta']
+            komp.nahraj_komponentu(komponenta())
+            return
+            
+        # Standardní navigace
+        komp = ziskej_komponentu()
+        # Sloučení výchozích parametrů z konfigurace s předanými parametry
+        vsechny_parametry = {**(konfig.get('parametry', {})), **parametry}
+        komp.nahraj_komponentu(konfig['komponenta'](**vsechny_parametry))
+        
+    except Exception as e:
+        logging.error(f"Chyba při navigaci na stránku {stranka}: {str(e)}")
+        alert("Došlo k chybě při navigaci")
+        naviguj('domu')
 
 def ziskej_komponentu():
     """
@@ -35,7 +180,7 @@ def ziskej_komponentu():
     if komponenta_hl_okna is None:
         raise Exception("Není zvolena žádná komponenta hlavního okna")
     return komponenta_hl_okna
-
+  
 def kontrola_prihlaseni():
     """
     Zkontroluje, zda je uživatel přihlášen, případně zobrazí přihlašovací dialog.
@@ -52,19 +197,9 @@ def kontrola_prihlaseni():
     komp.nastav_ucet(uzivatel)
     return uzivatel
 
-def set_active_nav(stav):
-    """
-    Označí aktivní položku v navigaci.
-    
-    Args:
-        stav: Identifikátor aktivní položky
-    """
-    komp = ziskej_komponentu()
-    komp.set_active_nav(stav)
-
 def over_a_smaz_rozpracovanou(cilova_stranka):
     """
-    Zkontroluje, zda v pravém panelu není rozdělaná analýza.
+    Zkontroluje, zda v hlavním panelu není rozdělaná analýza.
     
     Args:
         cilova_stranka: Identifikátor cílové stránky
@@ -91,7 +226,6 @@ def over_a_smaz_rozpracovanou(cilova_stranka):
                         return True
                     return False
                     
-                # Kontrola upravované analýzy
                 elif wizard.mode == Konstanty.STAV_ANALYZY['UPRAVA'] and wizard.mode != Konstanty.STAV_ANALYZY['ULOZENY']:
                     return confirm(Konstanty.ZPRAVY_CHYB['POTVRZENI_ZRUSENI_UPRAVY'],
                                  dismissible=True,
@@ -101,99 +235,3 @@ def over_a_smaz_rozpracovanou(cilova_stranka):
     except Exception as e:
         logging.error(f"Chyba při kontrole rozpracované analýzy: {str(e)}")
         return True
-
-# =============== Navigační funkce ===============
-
-def go_domu():
-    """Přejde na hlavní stránku."""
-    if over_a_smaz_rozpracovanou("domu"):
-        set_active_nav("domu")
-        komp = ziskej_komponentu()
-        uzivatel = Sprava_dat.je_prihlasen()
-        if uzivatel:
-            komp.nahraj_komponentu(Dashboard_uziv_komp())
-        else:
-            komp.nahraj_komponentu(HERO_komp())
-
-def go_pridat_analyzu():
-    """Přejde na stránku pro přidání nové analýzy."""
-    if over_a_smaz_rozpracovanou("pridat"):
-        set_active_nav("pridat")
-        uzivatel = kontrola_prihlaseni()
-        if not uzivatel:
-            go_domu()
-            return
-        
-        komp = ziskej_komponentu()
-        komp.nahraj_komponentu(Vyber_analyzy_komp())
-
-def go_upravit_analyzu():
-    """Přejde na stránku pro úpravu existující analýzy."""
-    set_active_nav("pridat")
-    uzivatel = kontrola_prihlaseni()
-    if not uzivatel:
-        go_domu()
-        return
-        
-    komp = ziskej_komponentu()
-    komp.nahraj_komponentu(Wizard_komp(mode=Konstanty.STAV_ANALYZY['UPRAVA']))
-
-def go_nastaveni():
-    """Přejde na stránku nastavení."""
-    if over_a_smaz_rozpracovanou("nastaveni"):
-        set_active_nav("nastaveni")
-        uzivatel = kontrola_prihlaseni()
-        if not uzivatel:
-            go_domu()
-            return
-        
-        komp = ziskej_komponentu()
-        komp.nahraj_komponentu(Nastaveni_komp())
-
-def go_info():
-    """Přejde na informační stránku."""
-    if over_a_smaz_rozpracovanou("info"):
-        set_active_nav("info")
-        komp = ziskej_komponentu()
-        komp.nahraj_komponentu(Info_komp())
-
-def go_administrace():
-    """Přejde na stránku administrace."""
-    if over_a_smaz_rozpracovanou("administrace"):
-        set_active_nav("administrace")
-        uzivatel = kontrola_prihlaseni()
-        if not uzivatel:
-            go_domu()
-            return
-        
-        komp = ziskej_komponentu()
-        komp.nahraj_komponentu(Administrace_komp())
-
-def go_ucet():
-    """Přejde na stránku uživatelského účtu."""
-    if over_a_smaz_rozpracovanou("ucet"):
-        uzivatel = kontrola_prihlaseni()
-        if not uzivatel:
-            go_domu()
-            return
-        
-        komp = ziskej_komponentu()
-        komp.nahraj_komponentu(Ucet_komp())
-
-def go_ahp():
-    """Přejde na stránku AHP analýzy."""
-    uzivatel = kontrola_prihlaseni()
-    if not uzivatel:
-        go_domu()
-        return
-    komp = ziskej_komponentu()
-    komp.nahraj_komponentu(Analyza_ahp_komp())
-
-def go_saw():
-    """Přejde na stránku SAW analýzy."""
-    uzivatel = kontrola_prihlaseni()
-    if not uzivatel:
-        go_domu()
-        return
-    komp = ziskej_komponentu()
-    komp.nahraj_komponentu(Wizard_komp())
