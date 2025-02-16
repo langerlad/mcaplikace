417c0e12de5fa9fbc00bfeef93eaa59970acc1aa
anvil.election653@passmail.net <anvil.election653@passmail.net>
2025-02-16 10:52:13 +0000

    Edited Dashboard_uziv_komp.Analyza_Row and Vystup_saw_komp

--- client_code/Vystup_saw_komp/__init__.py
+++ client_code/Vystup_saw_komp/__init__.py
@@ -6,57 +6,83 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 import anvil.users
 
-
 class Vystup_saw_komp(Vystup_saw_kompTemplate):
+  """
+  Stránka pro zobrazení vstupních dat a výsledků SAW analýzy.
+  Zobrazuje kompletní přehled analýzy včetně kritérií, variant a jejich hodnocení.
+  """
   def __init__(self, analyza_id=None, **properties):
-    # Set Form properties and Data Bindings.
+   
     self.init_components(**properties)
-
-    # Any code you write here will run before the form opens.
-    
-    # Instanční proměnné
     self.analyza_id = analyza_id
-    print(analyza_id)
 
   def form_show(self, **event_args):
-        if self.analyza_id:
+    """
+    Event handler vyvolaný při zobrazení formuláře.
+    Načte data analýzy a zobrazí je v připravených komponentách.
+    """
+    if self.analyza_id:
+        try:
             analyza_data = anvil.server.call('nacti_kompletni_analyzu', self.analyza_id)
-
             self.zobraz_vstup(analyza_data)
-
-            # spočítáme SAW scikit-criteria a výstup dáme do rich_text_vystupni_data
-            # "Zatím nenaimplementováno"
             self.rich_text_vystupni_data.content = "Výpočet SAW bude doplněn."
-        else:
-            self.rich_text_vstupni_data.text = "Nepřišlo žádné ID analýzy."
-            self.rich_text_vystupni_data.text = "Není co počítat."
+        except Exception as e:
+            alert(f"Chyba při načítání analýzy: {str(e)}")
+    else:
+        self.rich_text_vstupni_data.text = "Nepřišlo žádné ID analýzy."
+        self.rich_text_vystupni_data.text = "Není co počítat."
 
   def zobraz_vstup(self, analyza_data):
-      txt = f"""
-      **Název analýzy**: {analyza_data['analyza']['nazev']}
-      
-      **Popis**: {analyza_data['analyza']['popis']}
-      
-      **Metoda**: {analyza_data['analyza']['zvolena_metoda']}
-      """
+    """
+    Formátuje a zobrazuje vstupní data analýzy.
     
-      # Kritéria
-      txt += "\n\n**Kritéria**:\n"
-      for k in analyza_data['kriteria']:
-          txt += f"- {k['nazev_kriteria']} (typ: {k['typ']}, váha: {k['vaha']})\n"
+    Args:
+        analyza_data: Slovník obsahující kompletní data analýzy
+    """
+    # Základní informace o analýze
+    zakladni_info = f"""
+# {analyza_data['analyza']['nazev']}
 
-      # Varianty
-      txt += "\n**Varianty**:\n"
-      for v in analyza_data['varianty']:
-          txt += f"- {v['nazev_varianty']}: {v['popis_varianty']}\n"
+## Základní informace
+- **Metoda**: {analyza_data['analyza']['zvolena_metoda']}
+- **Popis**: {analyza_data['analyza']['popis'] or 'Bez popisu'}
 
-      # Hodnoty
-      txt += "\n**Hodnoty (matice_hodnoty)**:\n"
-      matice = analyza_data['hodnoty']['matice_hodnoty']
-      for klic, val in matice.items():
-          txt += f"- {klic} = {val}\n"
+## Kritéria
+| Název kritéria | Typ | Váha |
+|----------------|-----|------|
+"""
+    
+    # Přidání kritérií do tabulky
+    for k in analyza_data['kriteria']:
+        zakladni_info += f"| {k['nazev_kriteria']} | {k['typ']} | {k['vaha']} |\n"
 
-      self.rich_text_vstupni_data.content = txt
+    # Seznam variant
+    zakladni_info += "\n## Varianty\n"
+    for v in analyza_data['varianty']:
+        zakladni_info += f"- **{v['nazev_varianty']}**"
+        if v['popis_varianty']:
+            zakladni_info += f": {v['popis_varianty']}"
+        zakladni_info += "\n"
 
-      # Do rich_text_vystupni_data zatím nic nedáváme,
-      # vyplní se až při dalším kroku (např. s rozhodovací metodou).
+    # Vytvoření hodnotící matice
+    zakladni_info += "\n## Hodnotící matice\n"
+    
+    # Získání unikátních názvů variant a kritérií
+    varianty = [v['nazev_varianty'] for v in analyza_data['varianty']]
+    kriteria = [k['nazev_kriteria'] for k in analyza_data['kriteria']]
+    
+    # Hlavička tabulky
+    zakladni_info += "| Kritérium | " + " | ".join(varianty) + " |\n"
+    zakladni_info += "|" + "-|"*(len(varianty) + 1) + "\n"
+    
+    # Naplnění tabulky hodnotami
+    matice = analyza_data['hodnoty']['matice_hodnoty']
+    for krit in kriteria:
+        radek = f"| {krit} |"
+        for var in varianty:
+            klic = f"{var}_{krit}"
+            hodnota = matice.get(klic, "N/A")
+            radek += f" {hodnota} |"
+        zakladni_info += radek + "\n"
+
+    self.rich_text_vstupni_data.content = zakladni_info
