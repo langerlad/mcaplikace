4deb3fb246c9a02585cde52bc5a4d5608431fcdc
anvil.election653@passmail.net <anvil.election653@passmail.net>
2025-04-01 10:40:36 +0000

    Created Vystup_wsm_htm and HTML

---
+++ client_code/Vystup_wsm_htm/__init__.py
@@ -0,0 +1,16 @@
+from ._anvil_designer import Vystup_wsm_htmTemplate
+from anvil import *
+import plotly.graph_objects as go
+import anvil.server
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+
+class Vystup_wsm_htm(Vystup_wsm_htmTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
