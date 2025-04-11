ae455ba20e1605ac9838234c02998a7350935b86
anvil.election653@passmail.net <anvil.election653@passmail.net>
2025-04-11 20:31:28 +0000

    Edited Dashboard_uziv_komp.Row_dash

--- client_code/Dashboard_uziv_komp/Row_dash/__init__.py
+++ client_code/Dashboard_uziv_komp/Row_dash/__init__.py
@@ -13,3 +13,27 @@ class Row_dash(Row_dashTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def link_export_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
+
+  def link_vizualizace_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
+
+  def link_upravit_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
+
+  def link_klonovat_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
+
+  def link_json_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
+
+  def link_smazat_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
